46b3fe674fd30c22cf343d2c94c6dbf9b0cd7223
flights <60819474+flightsoffancy@users.noreply.github.com>
2022-07-19 17:21:10 -0400

    Add multiple round games

--- script.js
+++ script.js
@@ -2,9 +2,10 @@ const ROCK = "Rock";
 const PAPER = "Paper";
 const SCISSORS = "Scissors";
 const CHOICES = [ROCK, PAPER, SCISSORS];
+const GAME_ROUNDS = 5;
 
 function getChoice(selection) {
-    return CHOICES[selection]
+    return CHOICES[selection];
 }
 
 function getComputerChoice() {
@@ -42,7 +43,31 @@ function playRound(playerSelection, computerSelection) {
     return message;
 }
 
-const playerSelection = getChoice(0);
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
+function game() {
+    let playerWinCount = 0;
+    let computerWinCount = 0;
+    for (let i = 0; i < GAME_ROUNDS; i++) {
+        const userInput = prompt("Enter choice (rock: 0, paper: 1, scissors: 2): ");
+        const playerSelection = getChoice(+userInput);
+        const computerSelection = getComputerChoice();
+        let message = playRound(playerSelection, computerSelection);
+        if (message.includes("win")) {
+            playerWinCount++;
+        } else if (message.includes("lose")) {
+            computerWinCount++;
+        } else {
+            playerWinCount++;
+            computerWinCount++;
+        }
+        console.log(`Round ${i + 1}: ${message}`);
+    }
+    if (playerWinCount === computerWinCount) {
+        console.log("It's a tie!");
+    } else if (playerWinCount > computerWinCount) {
+        console.log("You win!");
+    } else {
+        console.log("You lose!");
+    }
+}
 
+game();
