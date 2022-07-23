1923a93e08a85562b2e6c318bb77927e1e8354a4
flights <60819474+flightsoffancy@users.noreply.github.com>
2022-07-23 19:26:42 -0400

    Add ui

--- script.js
+++ script.js
@@ -1,69 +1,80 @@
 const ROCK = 0;
 const PAPER = 1;
-const SCISSORS = 2
-const GAME_ROUNDS = 5;
+const SCISSORS = 2;
+
+buttons = document.querySelectorAll('.choices button');
+let roundCounter = 1;
+
+buttons.forEach((button) => {
+  button.addEventListener('click', () => {
+    if (!gameOver()) {
+      const playerSelection = parseInt(button.value);
+      const computerSelection = getComputerChoice();
+      const message = playRound(playerSelection, computerSelection);
+      let roundResultMessage = `Round ${roundCounter}: ${message}`;
+      const result = document.querySelector('#result');
+      // winner is null if there is a tie
+      const winner = determineWinner(message);
+      let score = null;
+      if (winner !== null) {
+        score = updateScore(winner);
+        if (score === 5) {
+          roundResultMessage = winner === 'player'
+                             ? 'You win! Refresh the page to restart'
+                             : 'You lose! Refresh the page to restart';
+        }
+      }
+      result.textContent = roundResultMessage;
+      roundCounter++;
+    }
+  });
+});
+
+function updateScore(winner) {
+  const scoreNode = document.querySelector(`.score #${winner}`);
+  newScore = parseInt(scoreNode.textContent) + 1;
+  scoreNode.textContent = newScore
+  return newScore;
+}
+
+function gameOver() {
+  const playerScore = document.querySelector('.score #player');
+  const computerScore = document.querySelector('.score #computer');
+  return parseInt(playerScore.textContent) === 5 
+         || parseInt(computerScore.textContent) === 5;
+}
 
 function getComputerChoice() {
   return Math.floor(Math.random() * 3);
 }
 
+function determineWinner(message) {
+  return message.includes('win') ? 'player'
+         : message.includes('lose') ? 'computer'
+         : null;
+}
+
 function playRound(playerSelection, computerSelection) {
   let message;
   if (playerSelection === computerSelection) {
-    message = "It's a tie!";
+    message = 'It\'s a tie!';
   } else {
     switch (playerSelection) {
       case ROCK:
         message = computerSelection === SCISSORS 
-                ? "You win! Rock beats Scissors"
-                : "You lose! Paper beats Rock";
+                ? 'You win! Rock beats Scissors'
+                : 'You lose! Paper beats Rock';
         break;
       case PAPER:
         message = computerSelection === ROCK
-                ? "You win! Paper beats Rock"
-                : "You lose! Paper beats Rock";
+                ? 'You win! Paper beats Rock'
+                : 'You lose! Paper beats Rock';
         break;
       case SCISSORS:
         message = computerSelection === PAPER
-                ? "You win! Scissors beats Paper"
-                : "You lose! Rock beats Scissors";
+                ? 'You win! Scissors beats Paper'
+                : 'You lose! Rock beats Scissors';
     }
   }
   return message;
 }
-
-function determineWinner(playerWinCount, computerWinCount) {
-  message = playerWinCount === computerWinCount ? "It's a tie!"
-          : playerWinCount > computerWinCount ? "You win!"
-          : "You lose!";
-  return message; 
-}
-
-function game() {
-  let playerWinCount = 0;
-  let computerWinCount = 0;
-  let round = 0;
-  while (round < GAME_ROUNDS) {
-    const userInput = prompt("Enter choice (rock: 0, paper: 1, scissors: 2): ");
-    if (isNaN(userInput) || userInput === null || userInput.trim() === "") {
-      console.log("user input must be an integer");
-      continue;
-    } else if (+userInput < 0 || +userInput > 2) {
-      console.log("valid choices are only (rock: 0, paper: 1, scissors: 2)");
-      continue;
-    }
-    const playerSelection = +userInput;
-    const computerSelection = getComputerChoice();
-    const message = playRound(playerSelection, computerSelection);
-    if (message.includes("win")) {
-      playerWinCount++;
-    } else if (message.includes("lose")) {
-      computerWinCount++;
-    } 
-    console.log(`Round ${round + 1}: ${message}`);
-    round++;
-  }
-  console.log(determineWinner(playerWinCount, computerWinCount)); 
-}
-
-game();
